519252fceb874596f536d208c654f6fea787ac7b
CamDawg <pcamagna@yahoo.com>
2022-09-28 06:09:13 -0700

    v11 prep

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Unfinished Business for Icewind Dale"
-set mod_version=v10
+set mod_version=v11
 set mod_folder=ub_iwd
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
